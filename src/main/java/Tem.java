c60bdb68289ec6475304e7095ca1bbd747def893
Piyaphat <piyaphat38030@gmail.com>
2026-08-21 12:48:48 +0800

    Rename Duke to Tem with greet and exit

---
+++ src/main/java/Tem.java
@@ -0,0 +1,16 @@
+public class Tem {
+    public static void main(String[] args) {
+        String banner = " _____                 \n"
+                + "|_   _|__ _ __ ___     \n"
+                + "  | |/ _ \\ '_ ` _ \\    \n"
+                + "  | |  __/ | | | | |   \n"
+                + "  |_|\\___|_| |_| |_|   \n";
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm Tem.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
